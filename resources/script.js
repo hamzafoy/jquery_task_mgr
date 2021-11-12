f06bdf9bc71bc0a7f69518a6b009d59a650ad91f
Ross Foy <hamzaoflouisville@gmail.com>
2021-11-12 05:12:42 -0500

    implement a way to edit a clicked task.

--- resources/script.js
+++ resources/script.js
@@ -1,12 +1,24 @@
 /* Click Event that helps detect double-clicks for mobile usage as well */
 let dbltouched = false;
-let editForm = `<input type="text" id="edit-field" name="edit-field" size="12" required>`
+let editForm = '<input type="text" id="edit-field" name="edit-field" size="12">';
 $('li').on('click', function() {
     let list = $(this).html();
+    console.log(list);
     if(list.indexOf('input') != -1) {
         console.log(`This is already in edit mode!`)
     } else {
-        $(this).append(editForm)
+        $(this).append(editForm);
+        $('#edit-field').on('keypress', function(e) {
+            e.stopPropagation;
+            let key = e.keyCode;
+            console.log($(this).val());
+            let formValue = $(this).val();
+            let formParent = $(this).parent();
+            console.log($(this).parent());
+            if (key == 13) {
+                formParent.replaceWith(`<li class="task-list__item">${formValue}</li>`);
+            }
+        });
     }
     if (dbltouched) {
         console.log(`Removing. . .`)
@@ -17,5 +29,8 @@ $('li').on('click', function() {
         dbltouched = false;
     }, 300);
 });
+
+
+
 
 $('#task-manager-screen').hide().fadeIn(1500);
