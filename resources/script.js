b50dc29beb38205ca88ff258f3d04e832678298a
Ross Foy <hamzaoflouisville@gmail.com>
2021-11-14 16:38:15 -0500

    bound the click events to document element and caused it to affect the list items in order for the events to be used on dynamically added content.

--- resources/script.js
+++ resources/script.js
@@ -1,15 +1,18 @@
+$(function() {
+
 /* Click Event that helps detect double-clicks for mobile usage as well */
 let dbltouched = false;
 let editForm = '<input type="text" id="edit-field" name="edit-field" size="12">';
-$('li').on('click', function() {
+let ulList = $('#task-manager__list');
+
+$(document).on('click', 'li', function() {
     let list = $(this).html();
-    console.log(list);
     if(list.indexOf('input') != -1) {
         console.log(`This is already in edit mode!`)
     } else {
         $(this).append(editForm);
         $('#edit-field').on('keypress', function(e) {
-            e.stopPropagation;
+            
             let key = e.keyCode;
             console.log($(this).val());
             let formValue = $(this).val();
@@ -34,13 +37,12 @@ $('li').on('click', function() {
 $('#add-task-form').on('submit', function(e) {
     e.preventDefault();
     let formValue = $('#addition-field').val();
-    let ulList = $('#task-manager__list');
     ulList.append(`<li class="task-list__item">${formValue}</li>`)
     console.log(formValue);
     console.log(ulList);
 });
 
 
-
-
 $('#task-manager-screen').hide().fadeIn(1500);
+
+});
