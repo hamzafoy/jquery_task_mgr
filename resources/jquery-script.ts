868cd744b85d6adee45e12b3591d4f800415699e
Ross Foy <hamzaoflouisville@gmail.com>
2021-11-22 00:52:12 -0500

    added a tsconfig file.

--- resources/jquery-script.ts
+++ resources/jquery-script.ts
@@ -2,5 +2,5 @@ $(function() {
 
     //Creating variable to store boolean value related to a li item being single- or double-clicked.
     let isADoubleClick: boolean = false;
-
+    let ulList = $('#task-manager__list');
 })
